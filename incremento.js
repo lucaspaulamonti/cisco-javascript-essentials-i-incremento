25e19ac3d8d3c5888adda23750a09a3b5a533c02
lucaspaulamonti <lucasdepaulamonti@gmail.com>
2022-10-18 00:07:11 -0300

    Create incremento.js

---
+++ incremento.js
@@ -0,0 +1,16 @@
+// Incrementos unários de valores.
+let n1 = 0;
+
+console.log(n1++);
+// É interpretado como:
+console.log(n1);
+n1 = n1 + 1;
+
+console.log(++n1);
+// É interpretado como:
+n1 = n1 + 1;
+console.log(n1);
+
+// Outra opção:
+n1 += 1;
+
